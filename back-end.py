a1a865d2e72e07a01ce6dbe86cfadc04d38ec3a7
Ryan Erickson <erickson.ryan.a@gmail.com>
2025-05-28 22:45:08 -0400

    Wrapped basic test API call in Flask server. Added functionality to request unique Steam ID based on given username. Next step is to remove hardcoding.

--- back-end.py
+++ back-end.py
@@ -1,10 +1,34 @@
+import re
 from dotenv import dotenv_values
 from requests import get
+from flask import Flask, render_template_string, redirect, url_for
 
+app = Flask(__name__)
+STEAM_API_KEY = dotenv_values(".env")["STEAM_API_KEY"]
+
+@app.route("/")
 def main():
-    STEAM_API_KEY = dotenv_values(".env")["STEAM_API_KEY"]
-    data = get("http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=440&format=json")
-    print(data.json())
+    username = ""
+    data = get(f"https://steamcommunity.com/id/{username}/")
+    
+    try:
+        # Capturing: "steamid":"012345"
+        # "Match" matches the entire string from the start. "Findall" searches
+        # for occurrences of the pattern in the string.
+        user_id = re.findall(r"\"steamid\":\"(\d+)\"", data.text)[0]
+    except:
+        print("\nERROR: No Steam ID captured.\n")
+    else:
+        print(f"\nCaptured Steam ID: {user_id}\n")
+        player_info = get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={user_id}&format=json")
+        print(player_info.json())
+    finally:
+        return render_template_string(data.text)
+
+@app.errorhandler(404)
+def handle_errors():
+    print("error detected.")
+    pass
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
