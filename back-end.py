f602935721fac0691c4603a0b30fe7042726b038
Ryan Erickson <erickson.ryan.a@gmail.com>
2025-06-02 21:19:44 -0400

    Re-did form submission to action, not onsubmit. JavaScript file removed for now. Testing Username to UID error handling.

--- back-end.py
+++ back-end.py
@@ -1,15 +1,40 @@
 import re
+import time
 from dotenv import dotenv_values
 from requests import get, exceptions
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 STEAM_API_KEY = dotenv_values(".env")["STEAM_API_KEY"]
+USER_CACHE = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/api/playerachievements", methods=["post"])
+def get_player_achievements():
+    username = request.form["username"]
+    print(username)
+    if username not in USER_CACHE:
+        print("user not in cache.\n")
+        uid = get_uid_from_username(username)
+        if uid == None:
+            return
+        USER_CACHE[username] = { 
+            "steam_id" : get_uid_from_username(username),
+            "last_accessed": time.time()
+        }
+        print(USER_CACHE[username])
+    else:
+        print("user found in cache!\n")
+        USER_CACHE[username]["last_accessed"] = time.time()
+        print(USER_CACHE[username])
+    
+    uid = USER_CACHE[username]["steam_id"]
+    list = get(f"http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key={STEAM_API_KEY}&steamid={uid}")
+    print(list.text)
+    return "Hello"
 
 def get_uid_from_username(username: str) -> str|None:
     try:
--- static/app.js
+++
@@ -1,6 +0,0 @@
-function submitUsername(event) {
-    event.preventDefault();
-    const id = document.getElementById("usernameInput");
-    console.log(id);
-    console.log("logged");
-}
