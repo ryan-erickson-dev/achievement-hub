d30638037886f5859d97c982526c9014e81744ad
Ryan Erickson <erickson.ryan.a@gmail.com>
2025-06-02 20:58:39 -0400

    Initial makings for restructured form submission, with JavaScript client-side handling server responses.

--- back-end.py
+++ back-end.py
@@ -1,7 +1,7 @@
 import re
 from dotenv import dotenv_values
-from requests import get
-from flask import Flask, request, render_template, render_template_string, redirect, url_for
+from requests import get, exceptions
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 STEAM_API_KEY = dotenv_values(".env")["STEAM_API_KEY"]
@@ -10,29 +10,22 @@ STEAM_API_KEY = dotenv_values(".env")["STEAM_API_KEY"]
 def index():
     return render_template("index.html")
 
-@app.route("/api/getuid", methods=["POST"])
-def main():
-    print("\n\n\nGIVEN USERNAME:", username := request.form["username"], "\n\n\n")
-    data = get(f"https://steamcommunity.com/id/{username}/")
-    
+
+def get_uid_from_username(username: str) -> str|None:
     try:
+        data = get(f"https://steamcommunity.com/id/{username}/")
+        data.raise_for_status()
+
         # Capturing: "steamid":"012345"
         # "Match" matches the entire string from the start. "Findall" searches
         # for occurrences of the pattern in the string.
-        user_id = re.findall(r"\"steamid\":\"(\d+)\"", data.text)[0]
-    except:
-        print("\nERROR: No Steam ID captured.\n")
+        uid = re.findall(r"\"steamid\":\"(\d+)\"", data.text)[0]
+    except exceptions.HTTPError as http_error:
+        print("ERROR:", http_error)
+        return None
     else:
-        print(f"\nCaptured Steam ID: {user_id}\n")
-        player_info = get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={user_id}&format=json")
-        print(player_info.json())
-    finally:
-        return render_template_string(data.text)
+        return uid
 
-# @app.errorhandler(404)
-# def handle_errors():
-#     print("error detected.")
-#     pass
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,6 @@
+function submitUsername(event) {
+    event.preventDefault();
+    const id = document.getElementById("usernameInput");
+    console.log(id);
+    console.log("logged");
+}
